05cea2917c5a87375ae735f233e1f58a68e00d16
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-08 16:41:22 -0500

    a few style changes, added __str__ to article

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ class Article(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 class Category(models.Model):
     title=models.CharField(max_length=56)
     articles=models.ManyToManyField(Article)
