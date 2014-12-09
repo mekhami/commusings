a7038f9b81e68f3d66e8446a9459d162f6623f2b
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-08 19:25:00 -0500

    removed save function from article model

--- blog/models.py
+++ blog/models.py
@@ -12,12 +12,6 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.slug = slugify(self.title)
-
-        super(Article, self).save(*args, **kwargs)
-
 class Category(models.Model):
     title=models.CharField(max_length=56)
     articles=models.ManyToManyField(Article)
