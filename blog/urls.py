76f2693a742c1913e49b4011c37cbcb2989d3755
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-08 19:17:32 -0500

    added migrations for article slug and teaser

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, patterns
 
-from .views import IndexView
+from .views import IndexView, ArticleView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
