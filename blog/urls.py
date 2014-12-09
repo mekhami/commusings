3696f2ec300a42e7d46f81596633cad2e3979033
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-08 19:45:58 -0500

    fixed urlconf

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import IndexView, ArticleView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<slug>[-_\w]+)/$', ArticleView.as_view(), name='article-view'),
+    url(r'^blog/article/(?P<slug>[\w\-]+)/$', ArticleView.as_view(), name='article-view'),
 )
