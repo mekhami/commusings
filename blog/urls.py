018e442772257cdeea6f500240f71c3c5d7805c1
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-08 19:40:39 -0500

    attempt 3

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import IndexView, ArticleView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<slug>[-_\w]+)/$', ArticleView.as_view(), name='article'),
+    url(r'^(?P<slug>[-_\w]+)/$', ArticleView.as_view(), name='article-view'),
 )
