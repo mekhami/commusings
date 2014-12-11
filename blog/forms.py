a1eeaaa05315129ffd2b09976ef6b56c3c18db19
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-10 20:59:47 -0500

    tied new views to include comment form

---
+++ blog/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    name = forms.CharField()
+    message = forms.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from blog.models import Article
+from django.views.generic import ListView, DetailView, View
+from blog.models import Article, Comment
+from blog.forms import ContactForm
+from django.core.urlresolvers import reverse
+
 
 # Create your views here.
 class IndexView(ListView):
@@ -8,7 +11,35 @@ class IndexView(ListView):
     context_object_name = 'articles'
     queryset = Article.objects.order_by("-pub_date")
 
-class ArticleView(DetailView):
+class ArticleDisplay(DetailView):
     template_name = 'article.html'
     model = Article
     context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleView, self).get_context_data(**kwargs)
+        context['form'] = ContactForm
+        return context
+
+class ArticleComment(SingleObjectMixin, FormView):
+    form_class = ContactForm
+    template_name = 'article.html'
+    model = Article
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super(CommentView, self).post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('article-view', kwargs={'pk': self.object.pk})
+
+class ArticleDetail(View):
+
+    def get(self, request, *args, **kwargs):
+        view = ArticleDisplay.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = ArticleComment.as_view()
+        return view(request, *args, **kwargs)
+
