e63ffb57d2178b64df4b152793e12943d6b44374
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-10 22:13:04 -0500

    added form class

--- blog/views.py
+++ blog/views.py
@@ -23,6 +23,7 @@ class ArticleDisplay(DetailView):
 class CreateCommentView(CreateView):
     model = Comment
     template_name = 'article.html'
+    form_class = CommentForm
 
     def dispatch(self, request, *args, **kwargs):
         self.article = get_object_or_404(Article, slug=kwargs['slug'])
