cfe2b9cc6d81ade7658d097a1caafacd92fa68ef
Lawrence Vanderpool <Lawrence.vanderpool@gmail.com>
2014-12-10 20:24:15 -0500

    added comments functionality

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,7 @@ class Comment(models.Model):
     user = models.CharField(max_length=56)
     subject = models.CharField(max_length=128)
     article = models.ForeignKey(Article)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user + ' ' + self.subject
