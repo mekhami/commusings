e3ed61e80d6c515916f0aab7d91e82a6d712f101
lawrencev <lawrence@zenoradio.com>
2014-12-13 14:26:50 -0500

    local configuration set up

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn commusings.wsgi --log-file -
-
--- commusings/settings.py
+++ commusings/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -17,10 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n369q%stgcqtn^nbou#&7im5gp&g=0zxefvn9ky0#%uz!&03=#'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG']
 
 TEMPLATE_DEBUG = True
 
@@ -57,17 +57,6 @@ WSGI_APPLICATION = 'commusings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dtjphhnkt98s6',
-        'HOST': 'ec2-107-20-245-187.compute-1.amazonaws.com',
-        'PORT': '5432',
-        'USER': 'hunnntcaccepxc',
-        'PASSWORD': 'J4fr6z3MRgFUCpQoShK3sRXOcG',
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -87,5 +76,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
