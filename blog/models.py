5186edfa1acb5ef91e16565214afe00eb5452d27
lawrencev <lawrence@zenoradio.com>
2014-12-15 21:42:55 -0500

    added automatic slugifying and cleaned up admin page

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    teaser = models.TextField()
+    teaser = models.CharField(max_length=200, default="", null=True)
     slug = models.SlugField()
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
 
 class Category(models.Model):
     title=models.CharField(max_length=56)
@@ -24,3 +29,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.user + ' ' + self.subject
+
